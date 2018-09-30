2e33ba6306b42a9abd439782db24b166b7506061
Vincent Harcq <vha@audaxis.com>
2018-09-30 10:17:36 +0200

    jupiter junit

--- src/test/java/org/vharcq/MainTestCase.java
+++ src/test/java/org/vharcq/MainTestCase.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import junit.framework.TestCase;
 
 class MainTestCase {
 
